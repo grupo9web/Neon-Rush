dece9123e7b88260b6bb09f2e6f556e942064089
ularrarte <32300902+ularrarte@users.noreply.github.com>
2019-05-21 18:03:00 +0200

    Ketsup o Kachup

---
+++ LEFT/Assets/ControlEmision.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlEmision : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Material material;
+
+    void Awake()
+    {
+        material.EnableKeyword ("_EMISSION");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
