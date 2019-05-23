16bf5be412e4845e58e42dc70428de5b453281c5
jpguirado <43378763+jpguirado@users.noreply.github.com>
2019-05-23 12:36:38 +0200

    Comienzo powerup

---
+++ NeonRush/Assets/PowerUps/saltoPowerUp.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class saltoPowerUp : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+    void OnCollisionEnter(Collision col)
+    {
+       
+        if (col.gameObject.name == "Player")
+        {
+            /*
+            col.gameObject.GetComponent<Actions>().increaseSpeed(speedGain);
+
+            int spawnPointIndex = Random.Range(0, spawnPoints.Length);
+            Instantiate(gameObject, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+            */
+
+            print("Funciona LOCOOOOOOOO");
+            Destroy(gameObject);
+
+        }
+    }
+}
